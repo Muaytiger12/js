c3ad16251ae3065caaf250f01ce11f2bb4325dcd
Maria <mariapiskunova51@gmail.comm>
2023-11-01 07:41:59 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,100 @@
+// let number = 5;
+// const leftBorderWidth = 1; 
+// number = 10;
+// console.log(number);
+// const obj = {
+// a:60
+// };
+// obj.a = 10;
+// console.log(obj)  // прямых констант в джс не бывает
+
+// "use strict"
+// a = 16;
+// console.log(a)
+
+// console.log(name);
+// var name = 'Ivan';
+
+// {
+
+//     let result =50;
+// }
+// console.log(result);
+
+// const widthAuto = 5000;
+// const heightAuto = 4000;
+
+// console.log('Ширина кузова авто '+widthAuto+', длина '+heightAuto);
+// ['csdjssd','dsdskjs','dnnjsdjsj'].map();
+// //Request 
+
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log(9*'string');
+
+// const person = `Alex`;
+// const bool = false;
+
+// console.log(something);
+// let und;
+// console.log(und);
+// const obj = {
+//     name: 'Alex',
+//     age: 25,
+//     isMarried: false
+// }
+// console.log(obj.age);
+
+// let arr = ['apple.png','orange.jpg','juc.bmp'];
+// console.log(arr[0]);
+
+// const arr = [1,2,3];
+
+// arr[10] ='34';
+// console.log(arr);
+
+// const arrObj = {
+//     a:'a',
+//     2:'b',
+//     3:'c',
+//     abc:{
+//         df:{
+            
+//         }
+//     }
+// }
+
+
+
+
+
+
+// const b = 'b';
+
+// // arrObj.b = '1234';
+// arrObj['b'] ='1234';
+// console.log(arrObj['b']);
+// ==============
+// const result = confirm('Are yo here?');
+// const answers = []
+
+// answers[0] = prompt('Name?', '')
+// answers[1] = prompt('Age?', '')
+// answers[2] = prompt('Job?', '')
+
+// console.log(typeof(answers));
+// console.log('arr'+' - object ')
+// console.log(4 + +'5')
+
+let incr =10, decr = 10;
+
+// ++incr
+// --decr
+// console.log(++incr)
+// console.log(--decr)
+// console.log(5%2)
+// console.log(2*4 === '8')
+
+const isCheked = true, isClose = false;
+console.log(isCheked || isClose)
